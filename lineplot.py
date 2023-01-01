f1b5a109ab4bbf59d87062afc25565f6c7dd4524
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-01-01 22:46:27 +0000

    added a scatter plot

--- lineplot.py
+++ lineplot.py
@@ -2,13 +2,16 @@
 
 # importing libraries
 import matplotlib.pyplot as plt
-from matplotlib import pyplot
-
 
 # the data and its variables
 year = [1950, 1970, 1990, 2010]
 # pop is world population in billions
 pop = [2.519, 3.692, 5.263, 6.972]
 
+# lineplot
 plt.plot(year, pop)
+plt.show()
+
+# scatterplot
+plt.scatter(year, pop)
 plt.show()
