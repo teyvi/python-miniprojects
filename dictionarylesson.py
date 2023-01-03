4a95f13f069dc64b944482bc6ab6f41c29d0f916
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-01-03 21:37:04 +0000

    added pandas to read the tsv file

---
+++ dictionarylesson.py
@@ -0,0 +1,23 @@
+# using indexing to select data
+
+# Definition of countries and capital
+countries = ['spain', 'france', 'germany', 'norway']
+capitals = ['madrid', 'paris', 'berlin', 'oslo']
+
+# Get index of 'germany': ind_ger
+ind_ger=countries.index("germany")
+
+# Use ind_ger to print out capital of Germany
+print(capitals[ind_ger])
+
+
+# creating a dictionary
+# Definition of countries and capital
+countries = ['spain', 'france', 'germany', 'norway']
+capitals = ['madrid', 'paris', 'berlin', 'oslo']
+
+# From string in countries and capitals, create dictionary europe
+europe = {'spain':'madrid','france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Print europe
+print(europe)
