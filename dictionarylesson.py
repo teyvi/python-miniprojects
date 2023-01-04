aec2ca5dea14c29ef65ee1f0e891e63d7b36ec2f
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-01-04 23:50:56 +0000

    created a manual dataframe

--- dictionarylesson.py
+++ dictionarylesson.py
@@ -5,7 +5,7 @@ countries = ['spain', 'france', 'germany', 'norway']
 capitals = ['madrid', 'paris', 'berlin', 'oslo']
 
 # Get index of 'germany': ind_ger
-ind_ger=countries.index("germany")
+ind_ger =countries.index("germany")
 
 # Use ind_ger to print out capital of Germany
 print(capitals[ind_ger])
@@ -21,3 +21,23 @@ europe = {'spain':'madrid','france':'paris', 'germany':'berlin', 'norway':'oslo'
 
 # Print europe
 print(europe)
+
+# creating a manual dataframe
+# importing libraries
+import pandas as pd
+
+names=['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
+dr=[True, False, False, False, True, True, True]
+cpc= [809, 731, 588, 18, 200, 70, 45]
+
+# creating a manual dictionary
+my_dict= {
+'names':['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt'],
+'dr':[True, False, False, False, True, True, True],
+'cpc': [809, 731, 588, 18, 200, 70, 45]
+}
+
+# building the dataframe called cars
+cars=pd.DataFrame(my_dict)
+
+print(cars)
