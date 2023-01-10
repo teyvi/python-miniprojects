8a92e8042eaa7a62d6347beffcf280620a6ba0fb
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-01-10 00:35:29 +0000

    coin toss with seed for reproducibility

---
+++ dice_stimulation.py
@@ -0,0 +1,23 @@
+# excercise 1
+# Import numpy and set seed
+import numpy as np
+np.random.seed(123)
+
+# Use randint() to simulate a dice
+coin= np.random.randint(1, 7)
+print(coin)
+# Use randint() again
+coin= np.random.randint(1, 7)
+print(coin)
+
+# change seed
+# Import numpy and set seed
+import numpy as np
+np.random.seed(124)
+
+# Use randint() to simulate a dice
+coin= np.random.randint(1, 7)
+print(coin)
+# Use randint() again
+coin= np.random.randint(1, 7)
+print(coin)
