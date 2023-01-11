79101949ce765590cf585e4635155ac95ce3dc8d
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-01-11 23:46:39 +0000

    coin toss with seed for reproducibility-exercise 2

--- dice_stimulation.py
+++ dice_stimulation.py
@@ -42,3 +42,25 @@ else :
 # Print out dice and step
 print(dice)
 print(step)
+
+
+# excersice 3
+# NumPy is imported, seed is set
+
+# Starting step
+step = 50
+
+# Roll the dice
+dice = np.random.randint(1,7)
+
+# Finish the control construct
+if dice <= 2 :
+    step = step - 1
+elif dice <=3 or dice <=5 :
+    step = step + 1
+else :
+    step = step + np.random.randint(1,7)
+
+# Print out dice and step
+print(dice)
+print(step)
