0d24a769eaf840122a019f6427ea047af630d81a
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-01-18 20:44:54 +0000

    timeseries visualization

--- gapmindervisualization.py
+++ gapmindervisualization.py
@@ -3,6 +3,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # read the tsv data
-data = pd.read_csv ("gapminder.tsv", sep= '\t')
+data = pd.read_csv ("data/gapminder.tsv", sep='\t')
 
 print(data)
--- timeseries_data.py
+++ timeseries_data.py
@@ -2,4 +2,4 @@
 import pandas as pd
 
 # Read the data from file using read_csv
-climate_change = pd.read_csv('climate_change.csv', parse_dates=['date'],index_col = 'date')
+climate_change = pd.read_csv('data/climate_change.csv', parse_dates=['date'],index_col = 'date')
