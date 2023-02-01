5b53a1d514e45018455e65f38d01da0ee6c1d664
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-02-01 23:39:17 +0000

    timeseries visualization - plotting two variables

--- timeseries_data.py
+++ timeseries_data.py
@@ -18,3 +18,20 @@ ax.set_ylabel('Relative temperature (Celsius)')
 # Show the figure
 plt.show()
 
+#plotting two variables
+import matplotlib.pyplot as plt
+
+# Initalize a Figure and Axes
+fig, ax = plt.subplots()
+
+# Plot the CO2 variable in blue
+ax.plot(climate_change.index, climate_change["co2"], color='blue')
+
+# Create a twin Axes that shares the x-axis
+ax2 = ax.twinx()
+
+# Plot the relative temperature in red
+ax2.plot(climate_change.index, climate_change["relative_temp"], color='red')
+
+plt.show()
+
