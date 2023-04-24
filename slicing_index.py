1f2281a917a71077ae8faa7a86447ae403624eab
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-04-24 22:40:18 +0000

    slicing index- changed dataframe and variable

--- slicing_index.py
+++ slicing_index.py
@@ -2,10 +2,10 @@
 temperatures_srt = temperatures_ind.sort_index()
 
 # Subset rows from Pakistan to Russia
-print(temperatures_ind.loc['Pakistan':'Russia'])
+print(temperatures_srt.loc['Pakistan':'Russia'])
 
 # Try to subset rows from Lahore to Moscow
-print(temperatures_ind.loc['Lahore':'Moscow']
+print(temperatures_srt.loc['Lahore':'Moscow'])
 
 # Subset rows from Pakistan, Lahore to Russia, Moscow
-print(temperatures_ind.loc[('Pakistan:Lahore'):('Russia':'Moscow')])
+print(temperatures_srt.loc[('Pakistan','Lahore'):('Russia','Moscow')])
