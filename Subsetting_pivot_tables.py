4889234fcdea408a1b7317a5cd1aafd10660ed4a
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-04-25 16:50:27 +0000

    subsetting pivot tables

---
+++ Subsetting_pivot_tables.py
@@ -0,0 +1,9 @@
+# Subset for Egypt to India
+temp_by_country_city_vs_year.loc['Egypt':'India']
+# Subset for Egypt, Cairo to India, Delhi
+temp_by_country_city_vs_year.loc[('Egypt','Cairo'):('India','Delhi')]
+
+# Subset for Egypt, Cairo to India, Delhi, and 2005 to 2010
+temp_by_country_city_vs_year.loc[('Egypt', 'Cairo'):('India', 'Delhi'), '2005':'2010']
+
+#source:https://campus.datacamp.com/courses/data-manipulation-with-pandas/slicing-and-indexing-dataframes?ex=13
--- pivot_table.py
+++ pivot_table.py
@@ -23,3 +23,4 @@ print(sales.pivot_table(values= 'weekly_sales', index= 'department' , columns= '
 
 # Print the mean weekly_sales by department and type; fill missing values with 0s; sum all rows and cols
 print(sales.pivot_table(values="weekly_sales", index="department", columns="type", fill_value=0, margins= True))
+
