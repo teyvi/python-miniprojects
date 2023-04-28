1c83fd5bac4dbde7271dcb71b2309c691853cb6f
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-04-28 01:55:32 +0000

    dataframes

--- dataframes.py
+++ dataframes.py
@@ -11,3 +11,12 @@ print(airline_bumping.head())
 
 # For each airline, select nb_bumped and total_passengers and sum
 airline_totals = airline_bumping.groupby('airline')[['nb_bumped','total_passengers']].sum()
+
+# From previous steps
+airline_bumping = pd.read_csv("airline_bumping.csv")
+print(airline_bumping.head())
+airline_totals = airline_bumping.groupby("airline")[["nb_bumped", "total_passengers"]].sum()
+airline_totals["bumps_per_10k"] = airline_totals["nb_bumped"] / airline_totals["total_passengers"] * 10000
+
+# Print airline_totals
+print(airline_totals)
