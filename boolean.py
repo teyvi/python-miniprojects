ecf3458b949c8b96a4b6bee5aa82ad62ed8e7920
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-11-18 21:36:30 +0000

    code to test booleans

---
+++ boolean.py
@@ -0,0 +1,9 @@
+#code to learn boolean
+#this simple code check if the door is open or not and prints a statement
+
+door_open_status = True
+
+if door_open_status:
+	print('its open')
+else:
+	print('its closed')
