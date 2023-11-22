15b3be0353a23240471da63edfb49f108697d4fa
teyvi <teyviangela@yahoo.com>
2023-11-22 23:48:25 +0000

    functions debugging

--- function_2.py
+++ function_2.py
@@ -3,3 +3,11 @@ def say_hi(name):
 
 print("Let's greet the entire world")
 say_hi('world')
+
+
+import sys
+if len(sys.argv) == 1:
+    name = sys.argv[1]
+else:
+    name = 'stranger'
+print(f'Hi there, {name}')
