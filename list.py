33d4a2dbf46f01c5ce17f42aacc6aeb837e2a62a
teyvi <teyviangela@yahoo.com>
2023-11-25 21:08:06 +0000

    learning list

---
+++ list.py
@@ -0,0 +1,11 @@
+#checks if an item is on a list
+
+my_list = [1,1,2]
+if 1 in my_list:
+    print("you got it here")
+else:
+    print('not in here')
+
+#find the index of a an item on a list
+my_list_2 = [1,2,3,4,5,6,]
+print(my_list_2.index(4))
