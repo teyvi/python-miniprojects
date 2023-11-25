e15497ed71e734346a438124b625dc17322013c3
teyvi <teyviangela@yahoo.com>
2023-11-25 22:00:02 +0000

    work with dictionaries

---
+++ dictionary.py
@@ -0,0 +1,26 @@
+#create a dictionary
+phone_numbers = {
+                    'mum' : '0234679124',
+                    'sist': '0923689235',
+                    'joh' : '9823579011',
+                }
+
+#print a phone number from list
+#print(phone_numbers['mum'])
+
+#delete phone number from list
+del(phone_numbers['sist'])
+#print(phone_numbers)
+
+#append the phonelist
+phone_numbers['Elton'] = '9388779348530'
+
+#print(phone_numbers)
+
+#overwrite a dictionary
+phone_numbers['mum'] = '0937499283568'
+
+'mum' in phone_numbers
+
+print(phone_numbers)
+
