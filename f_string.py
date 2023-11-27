999557da3c517f0859669e20ace3d783cad80590
teyvi <teyviangela@yahoo.com>
2023-11-27 22:27:32 +0000

    single and multiple string literal

---
+++ f_string.py
@@ -0,0 +1,10 @@
+#Modulo Operator
+FirstName = 'Angela'
+LastName = 'Teyvi'
+EmailSubject = 'Welcome to the team'
+
+#single variable in string literal
+#print('Hello, %s' % FirstName)
+
+b = 'Hello, Miss %s \n %s ! '% (LastName, EmailSubject)
+print(b)
